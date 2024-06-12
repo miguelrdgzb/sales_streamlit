3226c2d5e9dd99fe685c53d034ea162a70922bd9
Miguel Rodríguez Bubber <mrb.6393@gmail.com>
2024-06-12 14:46:05 +0200

    barchart, piechart

--- dashboard.py
+++ dashboard.py
@@ -34,4 +34,59 @@ with col2:
 
 df = df[(df['Order Date'] >= date1) & (df['Order Date'] <= date2)].copy()
 
-## 15.05
+st.sidebar.header("Elegir filtro: ")
+
+#Región
+
+region = st.sidebar.multiselect("Elegir región", df["Region"].unique())
+if not region:
+    df2 = df.copy()
+else:
+    df2 = df[df["Region"].isin(region)]
+
+
+#Estado
+
+state = st.sidebar.multiselect("Elegir Estado", df2["State"].unique())
+if not state:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["State"].isin(state)]
+
+#Ciudad
+
+city = st.sidebar.multiselect("Elegir Ciudad", df3["City"].unique())
+
+if not region and not state and not city:
+    filtered_df = df
+elif not state and not city:
+    filtered_df = df[df['Region'].isin(region)]
+elif not region and not city:
+    filtered_df = df[df['State'].isin(state)]
+elif not state and city:
+    filtered_df = df3[df['State'].isin(state) & df3['City'].isin(city)]
+elif not region and city:
+    filtered_df = df3[df['State'].isin(region) & df3['City'].isin(city)]
+elif not region and state:
+    filtered_df = df3[df['State'].isin(region) & df3['City'].isin(state)]
+elif city:
+    filtered_df = df3[df3['City'].isin(city)]
+else:
+    filtered_df = df3[df3['Region'].isin(region) & df3['State'].isin(state) & df3['City'].isin(city)]
+
+category_df = filtered_df.groupby(by = ['Category'], as_index=False)['Sales'].sum()
+
+with col1:
+    st.subheader('Ventas por categoría')
+    fig = px.bar(category_df, x = 'Category', y = 'Sales', text=['${:,.2f}'.format(x) for x in category_df['Sales']], 
+                 template='seaborn')
+    st.plotly_chart(fig, use_container_width=True, height = 200)
+
+with col2:
+    st.subheader('Ventas por Región')
+    fig = px.pie(filtered_df, values = 'Sales', names= 'Region', hole=0.5)
+    fig.update_traces(text = filtered_df['Region'], textposition= 'outside')
+    st.plotly_chart(fig, use_container_width=True)
+
+
+##29.23
